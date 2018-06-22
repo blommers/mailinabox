d93cd52be40cd10ae189dec323dcc5310bcd8c53
blommers <22526793+blommers@users.noreply.github.com>
2018-06-23 01:20:15 +0200

    disabled real starting of service
    disabled real starting of service, this should happen in a new docker

--- setup/start.sh
+++ setup/start.sh
@@ -112,53 +112,53 @@ source setup/management.sh
 source setup/munin.sh
 
 # Wait for the management daemon to start...
-until nc -z -w 4 127.0.0.1 10222
-do
-	echo Waiting for the Mail-in-a-Box management daemon to start...
-	sleep 2
-done
+# until nc -z -w 4 127.0.0.1 10222
+# do
+# 	echo Waiting for the Mail-in-a-Box management daemon to start...
+# 	sleep 2
+# done
 
-# ...and then have it write the DNS and nginx configuration files and start those
-# services.
-tools/dns_update
-tools/web_update
+# # ...and then have it write the DNS and nginx configuration files and start those
+# # services.
+# tools/dns_update
+# tools/web_update
 
-# Give fail2ban another restart. The log files may not all have been present when
-# fail2ban was first configured, but they should exist now.
-restart_service fail2ban
+# # Give fail2ban another restart. The log files may not all have been present when
+# # fail2ban was first configured, but they should exist now.
+# restart_service fail2ban
 
-# If DNS is already working, try to provision TLS certficates from Let's Encrypt.
-# Suppress extra reasons why domains aren't getting a new certificate.
-management/ssl_certificates.py -q
+# # If DNS is already working, try to provision TLS certficates from Let's Encrypt.
+# # Suppress extra reasons why domains aren't getting a new certificate.
+# management/ssl_certificates.py -q
 
-# If there aren't any mail users yet, create one.
-source setup/firstuser.sh
+# # If there aren't any mail users yet, create one.
+# source setup/firstuser.sh
 
-# Done.
-echo
-echo "-----------------------------------------------"
-echo
-echo Your Mail-in-a-Box is running.
-echo
-echo Please log in to the control panel for further instructions at:
-echo
-if management/status_checks.py --check-primary-hostname; then
-	# Show the nice URL if it appears to be resolving and has a valid certificate.
-	echo https://$PRIMARY_HOSTNAME/admin
-	echo
-	echo "If you have a DNS problem put the box's IP address in the URL"
-	echo "(https://$PUBLIC_IP/admin) but then check the TLS fingerprint:"
-	openssl x509 -in $STORAGE_ROOT/ssl/ssl_certificate.pem -noout -fingerprint -sha256\
-        	| sed "s/SHA256 Fingerprint=//"
-else
-	echo https://$PUBLIC_IP/admin
-	echo
-	echo You will be alerted that the website has an invalid certificate. Check that
-	echo the certificate fingerprint matches:
-	echo
-	openssl x509 -in $STORAGE_ROOT/ssl/ssl_certificate.pem -noout -fingerprint -sha256\
-        	| sed "s/SHA256 Fingerprint=//"
-	echo
-	echo Then you can confirm the security exception and continue.
-	echo
-fi
+# # Done.
+# echo
+# echo "-----------------------------------------------"
+# echo
+# echo Your Mail-in-a-Box is running.
+# echo
+# echo Please log in to the control panel for further instructions at:
+# echo
+# if management/status_checks.py --check-primary-hostname; then
+# 	# Show the nice URL if it appears to be resolving and has a valid certificate.
+# 	echo https://$PRIMARY_HOSTNAME/admin
+# 	echo
+# 	echo "If you have a DNS problem put the box's IP address in the URL"
+# 	echo "(https://$PUBLIC_IP/admin) but then check the TLS fingerprint:"
+# 	openssl x509 -in $STORAGE_ROOT/ssl/ssl_certificate.pem -noout -fingerprint -sha256\
+#         	| sed "s/SHA256 Fingerprint=//"
+# else
+# 	echo https://$PUBLIC_IP/admin
+# 	echo
+# 	echo You will be alerted that the website has an invalid certificate. Check that
+# 	echo the certificate fingerprint matches:
+# 	echo
+# 	openssl x509 -in $STORAGE_ROOT/ssl/ssl_certificate.pem -noout -fingerprint -sha256\
+#         	| sed "s/SHA256 Fingerprint=//"
+# 	echo
+# 	echo Then you can confirm the security exception and continue.
+# 	echo
+# fi
